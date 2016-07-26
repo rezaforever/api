9b1781c60d641cbb51837c04ca8ee0e79a618b73
rezaforever <rezaforever.rn@gmail.com>
2016-07-26 13:46:22 +0430

    Update wlc.lua

--- plugins/wlc.lua
+++ plugins/wlc.lua
@@ -1,11 +1,11 @@
 local add_user_cfg = load_from_file('data/add_user_cfg.lua')
 
-local function template_add_user(base, to_username, from_username, chat_name, chat_id)
+local function template_add_user(base, to_username, from_username, channel_name, channel_id)
    base = base or ''
    to_username = '@' .. (to_username or '')
    from_username = '@' .. (from_username or '')
-   chat_name = string.gsub(chat_name, '_', ' ') or ''
-   chat_id = "chat#id" .. (chat_id or '')
+   chaneel_name = string.gsub(channel_name, '_', ' ') or ''
+   channel_id = "channel#id" .. (channel_id or '')
    if to_username == "@" then
       to_username = ''
    end
@@ -14,31 +14,31 @@ local function template_add_user(base, to_username, from_username, chat_name, ch
    end
    base = string.gsub(base, "{to_username}", to_username)
    base = string.gsub(base, "{from_username}", from_username)
-   base = string.gsub(base, "{chat_name}", chat_name)
-   base = string.gsub(base, "{chat_id}", chat_id)
+   base = string.gsub(base, "{channel_name}", channel_name)
+   base = string.gsub(base, "{channel_id}", channel_id)
    return base
 end
 
-function chat_new_user_link(msg)
-   local pattern = add_user_cfg.initial_chat_msg
+function channel_new_user_link(msg)
+   local pattern = add_user_cfg.initial_channel_msg
    local to_username = msg.from.username
    local from_username = 'link (@' .. (msg.action.link_issuer.username or '') .. ')'
-   local chat_name = msg.to.print_name
-   local chat_id = msg.to.id
-   pattern = template_add_user(pattern, to_username, from_username, chat_name, chat_id)
+   local channel_name = msg.to.print_name
+   local channel_id = msg.to.id
+   pattern = template_add_user(pattern, to_username, from_username, channel_name, channel_id)
    if pattern ~= '' then
       local receiver = get_receiver(msg)
       send_msg(receiver, pattern, ok_cb, false)
    end
 end
 
-function chat_new_user(msg)
-   local pattern = add_user_cfg.initial_chat_msg
+function channel_new_user(msg)
+   local pattern = add_user_cfg.initial_channel_msg
    local to_username = msg.action.user.username
    local from_username = msg.from.username
-   local chat_name = msg.to.print_name
-   local chat_id = msg.to.id
-   pattern = template_add_user(pattern, to_username, from_username, chat_name, chat_id)
+   local channel_name = msg.to.print_name
+   local channel_id = msg.to.id
+   pattern = template_add_user(pattern, to_username, from_username, channel_name, channe_id)
    if pattern ~= '' then
       local receiver = get_receiver(msg)
       send_msg(receiver, pattern, ok_cb, false)
@@ -69,24 +69,24 @@ local function run(msg, matches)
    if not msg.service then
       return "Are you trying to troll me?"
    end
-   —vardump(msg)
-   if matches[1] == "chat_add_user" then
+   vardump(msg)
+   if matches[1] == "channel_add_user" then
       if not msg.action.user.username then
           nama = string.gsub(msg.action.user.print_name, "_", " ")
       else 
           nama = "@"..msg.action.user.username
       end
-      chat_new_user(msg)
+      channel_new_user(msg)
       description_rules(msg, nama)
-   elseif matches[1] == "chat_add_user_link" then
+   elseif matches[1] == "channel_add_user_link" then
       if not msg.from.username then
           nama = string.gsub(msg.from.print_name, "_", " ")
       else
           nama = "@"..msg.from.username
       end
-      chat_new_user_link(msg)
+      channel_new_user_link(msg)
       description_rules(msg, nama)
-   elseif matches[1] == "chat_del_user" then
+   elseif matches[1] == "channel_del_user" then
        local bye_name = msg.action.user.first_name
        return 'Bye Bye \n' ..bye_name 
    end
@@ -96,9 +96,9 @@ return {
    description = "Welcoming Message",
    usage = "send message to new member",
    patterns = {
-      "^!!tgservice (chat_add_user)$",
-      "^!!tgservice (chat_add_user_link)$",
-      "^!!tgservice (chat_del_user)$",
+      "^!!tgservice (channel_add_user)$",
+      "^!!tgservice (channel_add_user_link)$",
+      "^!!tgservice (channel_del_user)$",
    },
    run = run
 }
