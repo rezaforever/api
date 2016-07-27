b5e52ce2322608f008ce91c513759a19e0c9f070
rezaforever <rezaforever.rn@gmail.com>
2016-07-27 15:31:05 +0430

    Update sms.lua

--- plugins/sms.lua
+++ plugins/sms.lua
@@ -1,8 +1,5 @@
 local function run(msg, matches)
-  if not is_sudo(msg) then -- For Sudoers only !
-    return 'It is For Sudo Users'
-  end
- if matches[1] == 'sms' and is_sudo(msg) then
+ if matches[1] == 'sms' and is(msg) then
   local response = matches[3]
   send_large_msg("user#id"..matches[2], response)
       local receiver = 'user#id'..user_id
