bcbb61f463156d75e12af944d44645722c285738
rezaforever <rezaforever.rn@gmail.com>
2016-07-28 21:15:52 +0430

    Update and rename keep_calm.lua to keep_red.lua

--- plugins/keep_red.lua
+++ plugins/keep_red.lua
@@ -2,6 +2,7 @@ function run(msg, matches)
 		local text1 = matches[2]
         local text2 = matches[3]
 		local text3 = matches[4]
+        local color1 = matches[5]
 		local url = "http://www.keepcalmstudio.com/-/p.php?t=%EE%BB%AA%0D%0AKEEP%0D%0ACALM%0D%0A"..text1.."%0D%0A"..text2.."%0D%0A"..text3.."&bc=E31F17&tc=FFFFFF&cc=FFFFFF&uc=true&ts=true&ff=PNG&w=500&ps=sq"
 		 local  file = download_to_file(url,'keep.webp')
 			send_document(get_receiver(msg), file, ok_cb, false)
@@ -16,7 +17,7 @@ return {
     "/keep calm font text: Ø³Ø§Ø®Øª Ù„ÙˆÚ¯Ùˆ",
   },
   patterns = {
-   "^[/!]([Kk][Ee][Ee][Pp] [Cc][Aa][Ll][Mm]) (.+) (.+) (.+)$",
+   "^[/!]([Kk][Ee][Ee][Pp] [Rr][Ee][Dd]) (.+) (.+) (.+)$",
   },
   run = run
 }
