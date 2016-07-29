df8ac402eb9ab33487c8a1bd45bf60c512db80ad
rezaforever <rezaforever.rn@gmail.com>
2016-07-29 21:23:48 +0430

    Update time.lua

--- plugins/time.lua
+++ plugins/time.lua
@@ -5,15 +5,16 @@ if res ~= 200 then
  return "No connection"
   end
 local jdat = json:decode(url)
-local answers = {'https://assets.imgix.net/examples/clouds.jpg?blur=120&w=700&h=400&fit=crop&txt=',
+local answers = {
+                   'https://assets.imgix.net/examples/clouds.jpg?blur=120&w=700&h=400&fit=crop&txt=',
                    'https://assets.imgix.net/examples/redleaf.jpg?blur=120&w=700&h=400&fit=crop&txt=',
                    'https://assets.imgix.net/examples/blueberries.jpg?blur=120&w=700&h=400&fit=crop&txt=',
                    'https://assets.imgix.net/examples/butterfly.jpg?blur=120&w=700&h=400&fit=crop&txt=',
                    'https://assets.imgix.net/examples/espresso.jpg?blur=120&w=700&h=400&fit=crop&txt=',
                    'https://assets.imgix.net/unsplash/transport.jpg?blur=120&w=700&h=400&fit=crop&txt=',
-                   'https://assets.imgix.net/unsplash/coffee.JPG?blur=120&w=700&h=400&fit=crop&txt=',
+                   'https://assets.imgix.net/unsplash/coffee.jpg?blur=120&w=700&h=400&fit=crop&txt=',
                    'https://assets.imgix.net/unsplash/citystreet.jpg?blur=120&w=700&h=400&fit=crop&txt=',
-				   'http://assets.imgix.net/examples/vista.png?blur=120&w=700&h=400&fit=crop&txt='
+	           'https://assets.imgix.net/examples/vista.png?blur=120&w=700&h=400&fit=crop&txt='
 }
 
 local fonts = {'American%20Typewriter%2CBold','Arial%2CBoldItalicMT','Arial%2CBoldMT','Athelas%2CBold',
@@ -24,10 +25,10 @@ local fonts = {'American%20Typewriter%2CBold','Arial%2CBoldItalicMT','Arial%2CBo
 }
 
 local colors = {'00FF00','6699FF','CC99CC','CC66FF','0066FF','000000','CC0066','FF33CC','FF0000','FFCCCC','FF66CC','33FF00','FFFFFF','00FF00'}
-local f = fonts[matches.random(#fonts)]
-local c = colors[matches.random(#colors)]
+local f = fonts[math.random(#fonts)]
+local c = colors[math.random(#colors)]
 
-local url = answers[matches.random(#answers)]..jdat.ENtime.."&txtsize=200&txtclr="..c.."&txtalign=middle,center&txtfont="..f.."%20Condensed%20Medium&mono=ff6598cc=?markscale=60&markalign=center%2Cdown&mark64=QFVCX0NICg"
+local url = answers[math.random(#answers)]..jdat.ENtime.."&txtsize=200&txtclr="..c.."&txtalign=middle,center&txtfont="..f.."%20Condensed%20Medium&mono=ff6598cc=?markscale=60&markalign=center%2Cdown&mark64=QFVCX0NICg"
 local file = download_to_file(url,'time.webp')
 send_document(get_receiver(msg), file, ok_cb, false)
 end
