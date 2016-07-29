459ed505387794bb0c7da567c6f89ef01de563de
rezaforever <rezaforever.rn@gmail.com>
2016-07-30 00:18:46 +0430

    Update keep_calm.lua

--- plugins/keep_calm.lua
+++ plugins/keep_calm.lua
@@ -2,7 +2,7 @@ function run(msg, matches)
 		local text1 = matches[2]
         local text2 = matches[3]
 		local text3 = matches[4]
-        local url = "http://www.keepcalmstudio.com/-/p.php?t=%EE%BB%AA%0D%0AKEEP%0D%0ACALM%0D%0A"..text1.."%0D%0A"..text2.."%0D%0A"..text3.."&bc=..color..&tc=..color2..&cc=..c2..&uc=true&ts=true&ff=PNG&w=500&ps=sq"
+        local answer = {'http://www.keepcalmstudio.com/-/p.php?t=%EE%BB%AA%0D%0AKEEP%0D%0ACALM%0D%0A"..text1.."%0D%0A"..text2.."%0D%0A"..text3.."&bc=ffffff&tc=000000&cc=000000&uc=true&ts=true&ff=PNG&w=500&ps=sq','http://www.keepcalmstudio.com/-/p.php?t=%EE%BB%AA%0D%0AKEEP%0D%0ACALM%0D%0A"..text1.."%0D%0A"..text2.."%0D%0A"..text3.."&bc=000000&tc=ffffff&cc=ffffff&uc=true&ts=true&ff=PNG&w=500&ps=sq','http://www.keepcalmstudio.com/-/p.php?t=%EE%BB%AA%0D%0AKEEP%0D%0ACALM%0D%0A"..text1.."%0D%0A"..text2.."%0D%0A"..text3.."&bc=e31f17&tc=000000&cc=000000&uc=true&ts=true&ff=PNG&w=500&ps=sq','http://www.keepcalmstudio.com/-/p.php?t=%EE%BB%AA%0D%0AKEEP%0D%0ACALM%0D%0A"..text1.."%0D%0A"..text2.."%0D%0A"..text3.."&bc=ffffff&tc=000000&cc=000000&uc=true&ts=true&ff=PNG&w=500&ps=sq'}
         local c = {'FFFFFF','000000','E31F17','037D12','1B037B','0CC0FD','6A5DE3','E339B0','F7FF03'}
                 local c2 = {'FFFFFF','000000'}
         local color = c[math.random(#c)]
