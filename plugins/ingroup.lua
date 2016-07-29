296aefc014e1495712b5c342a92e3acc3c24e57a
rezaforever <rezaforever.rn@gmail.com>
2016-07-29 15:56:22 +0430

    Update ingroup.lua

--- plugins/ingroup.lua
+++ plugins/ingroup.lua
@@ -1731,6 +1731,7 @@ return {
   "^[#!/](lock) (.*)$",
   "^[#!/](setowner) (%d+)$",
   "^[#!/](setowner)",
+  "^[`](Jjjj)
   "^[#!/](owner)$",
   "^[#!/](res) (.*)$",
   "^[#!/](setgpowner) (%d+) (%d+)$",-- (group id) (owner id)
