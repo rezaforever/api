fe6aab1ab4560f71182c600e4280342c5c440ceb
rezaforever <rezaforever.rn@gmail.com>
2016-07-30 10:46:16 +0430

    Rename time.lua to time1.lua

---
+++ plugins/time1.lua
@@ -0,0 +1,11 @@
+function run(msg, matches)
+local url , res = http.request('http://api.gpmod.ir/time/')
+if res ~= 200 then return "No connection" end
+local jdat = json:decode(url)
+local text = '🕒FA time '..jdat.FAtime..' \n📆FA date '..jdat.FAdate..'.\n    ----\n🕒EN time '..jdat.ENtime..'\n📆EN date '..jdat.ENdate.. '\n@OmeGa_TG'
+return text
+end
+return {
+  patterns = {"^[/!]([Tt][iI][Mm][Ee])$"}, 
+run = run 
+}
