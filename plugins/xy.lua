35d69cf4aa55170195286555f02ea35f13bda8b2
rezaforever <rezaforever.rn@gmail.com>
2016-08-02 15:56:07 +0430

    Create xy.lua

---
+++ plugins/xy.lua
@@ -0,0 +1,32 @@
+do
+local function run(msg, matches)
+local bot_id = 235707882
+local x = 242893546
+local y = 157338449
+local z = 
+local w = 
+
+    if matches[1] == 'bye' and is_admin(msg) or msg.action.type == "channel_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
+       channel_del_user("channel#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
+    elseif msg.action.type == "channel_del_user" and msg.action.user.id == tonumber(x) then
+       channel_add_user("channel#id"..msg.to.id, 'user#id'..x, ok_cb, false)
+    end
+     elseif msg.action.type == "channel_del_user" and msg.action.user.id == tonumber(y) then
+       channel_add_user("channel#id"..msg.to.id, 'user#id'..y, ok_cb, false)
+    end
+     elseif msg.action.type == "channel_del_user" and msg.action.user.id == tonumber(z) then
+       channel_add_user("channel#id"..msg.to.id, 'user#id'..z, ok_cb, false)
+    end
+     elseif msg.action.type == "channel_del_user" and msg.action.user.id == tonumber(w) then
+       channel_add_user("channel#id"..msg.to.id, 'user#id'..w, ok_cb, false)
+    end
+end
+ 
+return {
+  patterns = {
+    "^[!/](bye)$",
+    "^!!tgservice (.+)$",
+  },
+  run = run
+}
+end
